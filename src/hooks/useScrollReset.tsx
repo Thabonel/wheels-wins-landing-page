90fef103599d1c8d74a499fcf997a00cc42078e2
thabonel0 <thabonel0@gmail.com>
2025-05-19 03:34:31 +0000

    fixed artifact on wheels

--- src/hooks/useScrollReset.tsx
+++ src/hooks/useScrollReset.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export const useScrollReset = () => {
+export const useScrollReset = (p0: string[]) => {
   const location = useLocation();
 
   useEffect(() => {
