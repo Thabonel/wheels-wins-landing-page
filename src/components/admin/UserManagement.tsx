5c5ac624d1f977f447586c467974d5e4d2f2b00c
Thabonel <thabonel0@gmail.com>
2025-06-08 06:38:39 +0000

    usermanagement

--- src/components/admin/UserManagement.tsx
+++ src/components/admin/UserManagement.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import { useAuth } from '@/context/AuthContext'
-import toast from 'react-hot-toast'
+import { toast } from '@/components/ui/use-toast'
 import {
   Card,
   CardHeader,
