e26609c3dc128d90736f98aa55d61602bbe95822
Thabonel <thabonel0@gmail.com>
2025-06-19 06:16:16 +0000

    update scraper

--- scraper_service/config.py
+++ scraper_service/config.py
@@ -8,9 +8,10 @@ DATA_SOURCES = [
         ],
     },
     {
-        "domain": "api.ioverlander.com",
+        "domain": "overpass-api.de",
         "urls": [
-            "https://api.ioverlander.com/api/campsites",
+            # Query free campsites within 50km of Sydney (lat -33.86, lon 151.21)
+            "https://overpass-api.de/api/interpreter?data=[out:json];node[\"tourism\"=\"camp_site\"](around:50000,-33.86,151.21);out;",
         ],
     },
 ]
