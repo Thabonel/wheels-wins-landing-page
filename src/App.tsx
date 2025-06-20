e6a6b6c026dbc751ed961349ffc0f00bd22ff2ae
Thabonel <thabonel0@gmail.com>
2025-06-20 23:55:11 +0000

    restore main

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'sonner';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { RegionProvider } from './context/RegionContext';
+import { OfflineProvider } from './context/OfflineContext';
+import { ExpensesProvider } from './context/ExpensesContext';
+import Layout from './components/Layout';
+import Index from './pages/Index';
+import Wheels from './pages/Wheels';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Onboarding from './pages/Onboarding';
+import You from './pages/You';
+import Wins from './pages/Wins';
+import Social from './pages/Social';
+import Shop from './pages/Shop';
+import AdminDashboard from './pages/AdminDashboard';
+import ScrollToTop from './components/ScrollToTop';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Layout from '@/components/Layout';
-import Home from '@/pages/Home';
-import You from '@/pages/You';
-import Wheels from '@/pages/Wheels';
-import Wins from '@/pages/Wins';
-import Social from '@/pages/Social';
-import Shop from '@/pages/Shop';
-import Profile from '@/pages/Profile';
-import Settings from '@/pages/Settings';
-import Admin from '@/pages/Admin';
-import LearningDashboard from '@/components/admin/LearningDashboard';
-import { AuthProvider } from '@/context/AuthContext';
-import { RegionProvider } from '@/context/RegionContext';
-import { WheelsProvider } from '@/context/WheelsContext';
-import { OfflineProvider } from '@/context/OfflineContext';
-import PamChatController from '@/components/pam/PamChatController';
-import { VoiceProvider } from '@/context/VoiceContext';
+import { Toaster } from 'sonner';
+import PaymentSuccess from "@/pages/PaymentSuccess";
+import PaymentCanceled from "@/pages/PaymentCanceled";
+import ProtectedRoute from './components/ProtectedRoute';
 
 const queryClient = new QueryClient();
 
@@ -27,35 +30,34 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
-      <AuthProvider>
-        <RegionProvider>
-          <WheelsProvider>
+      <Router>
+        <AuthProvider>
+          <RegionProvider>
             <OfflineProvider>
-              <VoiceProvider>
-                <BrowserRouter>
-                  <div className="min-h-screen bg-gray-50">
-                    <Layout>
-                      <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/you" element={<You />} />
-                        <Route path="/wheels" element={<Wheels />} />
-                        <Route path="/wins" element={<Wins />} />
-                        <Route path="/social" element={<Social />} />
-                        <Route path="/shop" element={<Shop />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/settings" element={<Settings />} />
-                        <Route path="/admin" element={<Admin />} />
-                        <Route path="/admin/learning" element={<LearningDashboard />} />
-                      </Routes>
-                    </Layout>
-                    <PamChatController />
-                  </div>
-                </BrowserRouter>
-              </VoiceProvider>
+              <ExpensesProvider>
+                <ScrollToTop />
+                <Layout>
+                  <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/wheels" element={<ProtectedRoute><Wheels /></ProtectedRoute>} />
+                    <Route path="/you" element={<ProtectedRoute><You /></ProtectedRoute>} />
+                    <Route path="/wins" element={<ProtectedRoute><Wins /></ProtectedRoute>} />
+                    <Route path="/social" element={<ProtectedRoute><Social /></ProtectedRoute>} />
+                    <Route path="/shop" element={<ProtectedRoute><Shop /></ProtectedRoute>} />
+                    <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/onboarding" element={<Onboarding />} />
+                    <Route path="/payment-success" element={<PaymentSuccess />} />
+                    <Route path="/payment-canceled" element={<PaymentCanceled />} />
+                  </Routes>
+                </Layout>
+              </ExpensesProvider>
             </OfflineProvider>
-          </WheelsProvider>
-        </RegionProvider>
-      </AuthProvider>
+          </RegionProvider>
+        </AuthProvider>
+      </Router>
     </QueryClientProvider>
   );
 }
