da1062a2328129936ad11f8a31be73f79a27b950
Thabonel <thabonel0@gmail.com>
2025-06-25 03:35:06 +0000

    ix the component usage:

--- src/components/PamSidebar.tsx
+++ src/components/PamSidebar.tsx
@@ -7,7 +7,7 @@ const PamSidebar: React.FC = () => {
     <div className="fixed right-0 top-32 h-[calc(100vh-8rem)] w-80 bg-white border-l border-gray-200 shadow-lg z-30">
       <div className="flex flex-col h-full">
         <div className="flex-1 overflow-hidden">
-          <PamAssistant />
+          <PamAssistantEnhanced />
         </div>
       </div>
     </div>
